7552cd63f57d3cf0f4075db76028c514d956947f
Ravazz <ravazziniandrea@hotmail.it>
2024-06-24 00:01:13 +0200

    Added main TODO list

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+
+/** TODO LIST:
+     - read file to get paths to backup
+     - "procedural" backup (like rsync)
+     - create a tarball
+     - checksum to get tarballs differences
+     - push to Google Drive (?)
+**/
+
+
+
 fn main() {
     println!("One day, I'll be a cool backup utility :)");
 }
