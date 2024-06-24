e8e4f37420cc1dc72501b1da7481fdb249ac9dcc
AndreaR <andrea.ravazzini@zanasicoding.com>
2024-06-24 13:47:18 +0200

    Added arguments check

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::env;
+use std::path::Path;
 
 /** TODO LIST:
      - read file to get paths to backup
@@ -7,8 +9,30 @@
      - push to Google Drive (?)
 **/
 
+fn file_exists(path: &str) -> bool {
+    Path::new(path).exists()
+}
 
 
 fn main() {
     println!("One day, I'll be a cool backup utility :)");
+
+    let args: Vec<String> = env::args().collect();
+    //dbg!(args);
+    let mut iter = args.iter();
+    let script_path = iter.next();
+    let mut file_path = "";
+    while let Some(arg) = iter.next(){
+        match arg.as_str() {
+            "-f" => {
+                file_path = iter.next().expect("Path not provided");
+                if file_exists(file_path) {
+                    println!("EXIXSTE")
+                } else {
+                    panic!("{} does not exists. Abort", file_path);
+                }
+            }
+            _ => {println!("{} not a valid argument", arg)}
+        }
+    }
 }
