f004fe12ed406b1ef22e0a1b5c3386a26b041256
AndreaR <andrea.ravazzini@zanasicoding.com>
2024-06-28 08:24:21 +0200

    Squashed commit of the following:
    commit 5a711c05f2ccdd21a9137cd17f5770ee20169094
    Author: AndreaR <andrea.ravazzini@zanasicoding.com>
    Date:   Fri Jun 28 08:23:45 2024 +0200
    
        First minor milestone
    
    commit 4db2d6c99785a8cdc9d2e9a2768b64b01048fb4b
    Author: Ravazz <ravazziniandrea@hotmail.it>
    Date:   Thu Jun 27 20:29:12 2024 +0200
    
        Starting to add rsync
    
    commit 41138142137ec52d62e668ad351362d6d0d89636
    Author: AndreaR <andrea.ravazzini@zanasicoding.com>
    Date:   Thu Jun 27 17:04:28 2024 +0200
    
        started to parse toml
    
    commit c7637e2842910560e8d2d3c3a88df4e2d1bca728
    Author: AndreaR <andrea.ravazzini@zanasicoding.com>
    Date:   Tue Jun 25 11:39:13 2024 +0200
    
        Added home dir to paths
    
    commit c57d9d5661c7311b7c6ed6633ef382c373c9b51d
    Author: AndreaR <andrea.ravazzini@zanasicoding.com>
    Date:   Mon Jun 24 17:04:22 2024 +0200
    
        Added file parser
    
    commit e8e4f37420cc1dc72501b1da7481fdb249ac9dcc
    Author: AndreaR <andrea.ravazzini@zanasicoding.com>
    Date:   Mon Jun 24 13:47:18 2024 +0200
    
        Added arguments check

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,111 @@
+use std::{env, fs};
+use std::path::{Path, PathBuf};
+use dirs;
+use serde::Deserialize;
+use toml::Table;
 
-/** TODO LIST:
-     - read file to get paths to backup
-     - "procedural" backup (like rsync)
-     - create a tarball
-     - checksum to get tarballs differences
-     - push to Google Drive (?)
+/**
+     - {DONE} read file to get paths to backup
+     - {DONE} read config file
+     - [TODO] exclude files
+     - [TODO] create a tarball
+     - [TODO] checksum to get tarballs differences
+     - [TODO] push to Google Drive (?)
 **/
 
+#[derive(Deserialize)]
+struct Config {
+    path: String
+}
 
+const CONFIG_FILE: &str = "./config.toml";
+
+
+fn path_exists(path: &str) -> bool {
+    Path::new(path).exists()
+}
+
+
+fn read_files(path: &str) -> Vec<String>{
+    let mut files = Vec::new();
+    for line in fs::read_to_string(path).unwrap().lines(){
+        files.push(line.to_string());
+    }
+    files
+}
+
+
+fn real_lines(lines: Vec<String>) -> Vec<String>{
+    let mut reals = Vec::new();
+    let home_dir = dirs::home_dir().unwrap_or_else(|| PathBuf::from("."));
+
+    for line in lines{
+        let line = if line.starts_with("~"){
+            line.replace("~", home_dir.to_str().unwrap())
+        } else {
+            line
+        };
+        if path_exists(line.as_str()){
+            reals.push(line);
+        }
+    }
+    reals
+}
+
+
+fn path_exists_or_exit(path: Option<&String>){
+    if let Some(path) = path {
+        if !path_exists(path.as_str()) {
+            eprintln!("Error: {} does not exists. Abort", path);
+            std::process::exit(1);
+        }
+    } else {
+        eprintln!("Error. No file path provided. Abort");
+        std::process::exit(1);
+    }
+}
+
+
+fn parse_config_file() {
+    path_exists_or_exit(Some(&CONFIG_FILE.to_string()));
+
+    let config_data = fs::read_to_string(CONFIG_FILE).unwrap();
+    dbg!("{}", config_data);
+    let val = config_data.parse::<Table>().unwrap();
+    //let config: Config = toml::from_str(config_data.as_str()).expect("Cant read config file :(");
+
+    dbg!("{}", val["location"]["path"]);
+}
+
+fn rsync_files() {
+    librsync::whole
+}
 
 fn main() {
     println!("One day, I'll be a cool backup utility :)");
+
+    let args: Vec<String> = env::args().collect();
+    //dbg!(args);
+    let mut iter = args.iter();
+    let _script_path = iter.next();
+    let mut file_path = None;
+    while let Some(arg) = iter.next() {
+        match arg.as_str() {
+            "-f" => {
+                file_path = Some(iter.next().expect("Path not provided"));
+            }
+            _ => {eprintln!("{} not a valid argument", arg)}
+        }
+    }
+
+    //  ---- READ CONFIG FILE -------------------------------------------
+    parse_config_file();
+
+    path_exists_or_exit(file_path);
+
+    let lines = read_files(file_path.unwrap());
+    let paths_to_store = real_lines(lines);
+    dbg!("{:?}", paths_to_store);
+
+    rsync_files();
 }
